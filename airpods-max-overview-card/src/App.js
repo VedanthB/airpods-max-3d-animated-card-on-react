6fed5821b189d075d7be6032d72194526cd7043c
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-19 00:41:00 +0530

    app.js

--- airpods-max-overview-card/src/App.js
+++ airpods-max-overview-card/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import { AirpodsCard } from "./component/airpodsCard";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <AppContainer>
+      <AirpodsCard />
+    </AppContainer>
   );
 }
 
